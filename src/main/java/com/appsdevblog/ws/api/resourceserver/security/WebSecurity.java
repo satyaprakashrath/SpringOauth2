1568f743e952f85b5944333b3c57baada9a23d49
satya <satyaprakash.rath@clarivate.com>
2023-07-14 10:45:44 +0530

    first commit

---
+++ src/main/java/com/appsdevblog/ws/api/resourceserver/security/WebSecurity.java
@@ -0,0 +1,54 @@
+package com.appsdevblog.ws.api.resourceserver.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+    /*@Bean
+    SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        System.out.println("Request coming here");
+        httpSecurity
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }));
+
+        return httpSecurity.build();
+    }*/
+
+    /*@Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .antMatchers(HttpMethod.GET,"/users/status").hasAuthority("SCOPE_profile")
+                        .anyRequest().authenticated()
+                ).oauth2ResourceServer((oauth2ResourceServer) ->
+                        oauth2ResourceServer
+                                .jwt(withDefaults())
+                );
+
+        return http.build();
+    }*/
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleCoverter());
+        http
+                .authorizeHttpRequests().antMatchers(HttpMethod.GET,"/users/status").hasRole("developer")
+                .anyRequest().authenticated().and().oauth2ResourceServer().jwt().jwtAuthenticationConverter(converter);
+
+        return http.build();
+    }
+}
---
+++ src/test/java/com/appsdevblog/ws/api/resourceserver/ResourceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdevblog.ws.api.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
